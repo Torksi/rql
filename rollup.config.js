7970aebf49fac3fa9761708d5cab8d3dcd97f035
Toni Ruhanen <toni@ruhis.fi>
2023-06-13 17:32:43 +0300

    Fixed type definitions

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,17 @@
-export default {
-  input: "dist/index.js",
-  output: {
-    file: "index.js",
+import dts from "rollup-plugin-dts";
+
+export default [
+  {
+    input: "dist/index.js",
+    output: {
+      file: "index.js",
+    },
   },
-};
+  {
+    input: "dist/index.d.ts",
+    output: {
+      file: "index.d.ts",
+    },
+    plugins: [dts()],
+  },
+];
